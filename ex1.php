12a30bdfcfd3ae9481d5aff82837be07e3b1c38f
katiabeloto <katia.beloto@hotmail.com>
2024-05-22 22:08:29 -0300

    Atividade 29/05/24

---
+++ ex1.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Variáveis entre aspas "" são do tipo String/Linha. Variáveis numéricas fora de aspas podem ser int(inteiros) (10) ou float(decimais)(10.0)
+$alunos = ("Thamires", "Vanessa", "Andrea", "Bruna","Júlia", "Katia", "Maria","Larissa", "Alice", "Luzinete")
+$notas = (7,5,8,9,7,10,9,9,10,10)
+//O soma das notas é 84, logo a média é 8.4
+$media = 0
+$soma = 0
+// A variável abaixo controla de que posição no array/vetor o valor será armazenado em $soma
+$pos = 0
+
+
+
+ ?>
